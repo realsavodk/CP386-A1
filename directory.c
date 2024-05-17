d1dafeda7e00ad285f6882d9cc6d45acb29c395e
Jake Watts <jaketimothywatts@gmail.com>
2024-05-17 10:11:09 -0400

    Get basic menu functionality working.
    Currently, each function just prints a string to the terminal. Next I'll
    start implementing these.

--- directory.c
+++ directory.c
@@ -1,14 +1,38 @@
 /*
-* 1. Create a directory (`mkdir`)
-* 2. Remove a directory (`rm -rf`)
-* 3. Get the current working directory (`pwd`)
-* 4. Changing to the parent directory (`..`)
-* 5. Reading the contents of the current directory (`ls`)
-* 6. Closing the opened directory (I can't think of a similar command)
-*/
+ * 1. Create a directory (`mkdir`)
+ * 2. Remove a directory (`rm -rf`)
+ * 3. Get the current working directory (`pwd`)
+ * 4. Changing to the parent directory (`..`)
+ * 5. Reading the contents of the current directory (`ls`)
+ * 6. Closing the opened directory (I can't think of a similar command)
+ */
 
 #include <stdio.h>
+#include <stdlib.h>
 
+void create_directory() {
+    printf("Create directory!\n\n");
+}
+
+void remove_directory() {
+    printf("Remove directory!\n\n");
+}
+
+void get_current_directory() {
+    printf("Get current directory!\n\n");
+}
+
+void change_to_parent_directory() {
+    printf("Change to parent directory!\n\n");
+}
+
+void read_directory() {
+    printf("Read directory (ls)!\n\n");
+}
+
+void close_directory() {
+    printf("Close directory (what does that even mean?)\n\n");
+}
 
 char display_menu() {
     char choice;
@@ -24,13 +48,36 @@ char display_menu() {
     return choice;
 }
 
-
 int main() {
-
     while (1) {
         char choice = display_menu();
-        printf("Your choice: %c\n\n", choice);
-    } 
+        switch (choice) {
+            case '1':
+                create_directory();
+                break;
+            case '2':
+                remove_directory();
+                break;
+            case '3':
+                get_current_directory();
+                break;
+            case '4':
+                change_to_parent_directory();
+                break;
+            case '5':
+                read_directory();
+                break;
+            case '6':
+                close_directory();
+                break;
+            case 'q':
+                exit(0);
+                break;
+            default:
+                printf("Invalid choice, please try again!\n\n");
+        }
+
+    }
 
     return 0;
 }
